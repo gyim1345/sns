e855739c669476103dd9bbce02ca145a08f6be45
gyim <gyim1345@gmail.com>
2020-05-12 20:48:49 +0900

    test

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist/'),
     filename: 'main.js'
